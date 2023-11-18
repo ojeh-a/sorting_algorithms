598c45512fdb42adecd0a99f86b56149ea0eeafd
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-11-18 13:24:29 -0800

    sort

--- sort.h
+++ sort.h
@@ -21,8 +21,11 @@ typedef struct listint_s
 /* print_array */
 void print_array(const int *array, size_t siz);
 
+
 /* print_list */
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
 /* ALGORITHMS */
 void bubble_sort(int *array, size_t size); /* bubble sort */
