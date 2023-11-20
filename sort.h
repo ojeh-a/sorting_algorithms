29fb96b83c69450a020bea85f22369d91ac68ee7
Ojeh <ojehdayo@gmail.com>
2023-11-20 06:29:51 +0100

    counting sort

--- sort.h
+++ sort.h
@@ -23,10 +23,6 @@ typedef struct listint_s
 /* print_array */
 void print_array(const int *array, size_t siz);
 void swap_ints(int *a, int *b);
-void quick_sort(int *array, size_t size);
-/* void lomuto_so; */
-int lomuto_partition(int *array, size_t size, int left, int right);
-void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 
@@ -36,16 +32,17 @@ void print_list(const listint_t *list);
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
 /* ALGORITHMS */
-void bubble_sort(int *array, size_t size); /* bubble sort */
-void insertion_sort_list(listint_t **list); /* insertion sort */
-void selection_sort(int *array, size_t size); /* selection sort */
-void quick_sort(int *array, size_t size); /* quick sort */
-void shell_sort(int *array, size_t size); /* shell sort */
-void cocktail_sort_list(listint_t **list); /* cocktail shaker sort */
-void counting_sort(int *array, size_t size); /* counting sort */
-void merge_sort(int *array, size_t size); /* merge sort */
-void heap_sort(int *array, size_t size); /* heap sort */
-void radix_sort(int *array, size_t size); /*radix sort */
-void bitonic_sort(int *array, size_t size); /* bitonic sort */
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 
 #endif
